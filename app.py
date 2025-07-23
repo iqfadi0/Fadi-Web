42fb25dd268705bac0768e6a9e123c923f3c7513
iqfadi0 <iqxf40@gmail.com>
2025-07-24 00:30:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -86,4 +86,5 @@ def send_daily_reminder():
 threading.Thread(target=send_daily_reminder, daemon=True).start()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
