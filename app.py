f5d925c60aea99a7e60b595d376a46e83abff3c3
iqfadi0 <iqxf40@gmail.com>
2025-07-23 22:36:11 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, render_template, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/subscribers")
+def subscribers():
+    with open("subscribers.json", "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
